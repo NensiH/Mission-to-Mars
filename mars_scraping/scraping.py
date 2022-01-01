357be94bca3faf0035b5527985930dae03610f3c
nensi <NENSI.DOBARIA@GMAIL.COM>
2022-01-01 17:11:20 -0600

    challenge 10 progress

--- mars_scraping/scraping.py
+++ mars_scraping/scraping.py
@@ -92,7 +92,7 @@ def mars_facts():
 
     # Assign columns and set index of dataframe
     df.columns=['Description', 'Mars', 'Earth']
-    df.set_index('Description', inplace=True)
+    # df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
